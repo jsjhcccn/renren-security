c136d277335aefec2cb88f86d38c094a674fcc4e
unknown <jsjhcccn@hotmail.com>
2019-05-19 22:36:36 +0800

    iot

---
+++ renren-api/src/main/java/io/renren/controller/IndexController.java
@@ -0,0 +1,19 @@
+package io.renren.controller;
+
+import io.renren.common.utils.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@Api(tags="首页接口")
+public class IndexController {
+    @GetMapping("notToken")
+    @ApiOperation("忽略Token验证测试")
+    public R notToken(){
+        return R.ok().put("msg", "无需token也能访问。。。");
+    }
+}
